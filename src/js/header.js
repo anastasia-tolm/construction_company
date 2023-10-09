261b5e5c9be5d58718ec1a7d7e6938949b23bea8
Anastasia Tolmacheva <anastasia.pnz@inbox.ru>
2023-10-09 15:04:51 +0300

    header: add js

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,11 @@
 import '../styles/components/header.scss';
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('body_lock');
+    iconMenu.classList.toggle('menu__icon_active');
+    menuBody.classList.toggle('menu__body_active');
+  });
+}
