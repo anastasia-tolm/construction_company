0b8a2045ea48dacb522f9c49b830f59451cb84a2
Anastasia Tolmacheva <anastasia.pnz@inbox.ru>
2023-10-10 18:39:38 +0300

    fixed media queries and added script for spoiler

--- src/js/header.js
+++ src/js/header.js
@@ -1,11 +1,35 @@
 import '../styles/components/header.scss';
 
+// Бургер
+
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
+const menuBodyTablet = document.querySelector('.menu__body-tablet');
 if (iconMenu) {
-  iconMenu.addEventListener('click', function (e) {
+  iconMenu.addEventListener('click', function () {
     document.body.classList.toggle('body_lock');
     iconMenu.classList.toggle('menu__icon_active');
     menuBody.classList.toggle('menu__body_active');
+    menuBodyTablet.classList.toggle('menu__body-tablet_active');
   });
 }
+
+// Спойлер
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuItems = document.querySelectorAll('.menu__item-spoiler');
+
+  menuItems.forEach((menuItem) => {
+    const submenuToggle = menuItem.querySelector('a');
+
+    submenuToggle.addEventListener('click', (event) => {
+      event.preventDefault(); // Отменяем стандартное действие ссылки
+      menuItem
+        .querySelector('.menu__sublist')
+        .classList.toggle('menu__sublist_active');
+      menuItem
+        .querySelector('.menu__arrow')
+        .classList.toggle('menu__arrow_active');
+    });
+  });
+});
