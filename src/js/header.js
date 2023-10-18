a9ec6d13dfecd7a2e1839dba2e66ffbd5129a67c
Anastasia Tolmacheva <anastasia.pnz@inbox.ru>
2023-10-18 18:45:41 +0300

    change :focus

--- src/js/header.js
+++ src/js/header.js
@@ -1,32 +1,33 @@
 import '../styles/components/header.scss';
 
-// Бургер
-
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
-const menuBodyTablet = document.querySelector('.menu__body-tablet');
-if (iconMenu) {
-  iconMenu.addEventListener('click', function () {
-    document.body.classList.toggle('body_lock');
-    iconMenu.classList.toggle('menu__icon_active');
-    menuBody.classList.toggle('menu__body_active');
-    menuBodyTablet.classList.toggle('menu__body-tablet_active');
-  });
+function hideShowMenu() {
+  const iconMenu = document.querySelector('.menu__icon');
+  const menuBody = document.querySelector('.menu__body');
+  const menuBodyTablet = document.querySelector('.menu__body-tablet');
+  if (iconMenu) {
+    iconMenu.addEventListener('click', function () {
+      document.body.classList.toggle('body_lock');
+      iconMenu.classList.toggle('menu__icon_active');
+      menuBody.classList.toggle('menu__body_active');
+      menuBodyTablet.classList.toggle('menu__body-tablet_active');
+    });
+  }
 }
 
-// Спойлер
-
-if (document.documentElement.clientWidth < 767) {
-  document.addEventListener('DOMContentLoaded', () => {
-    const menuArrows = document.querySelectorAll('.menu__arrow');
-    const menuSublists = document.querySelectorAll('.menu__sublist');
-    if (menuArrows.length > 0) {
-      menuArrows.forEach((menuArrow, index) => {
-        menuArrow.addEventListener('click', () => {
-          menuArrow.classList.toggle('menu__arrow_active');
-          menuSublists[index].classList.toggle('menu__sublist_active');
-        });
+function hideShowSpoiler() {
+  const menuArrows = document.querySelectorAll('.menu__arrow');
+  const menuSublists = document.querySelectorAll('.menu__sublist');
+  if (menuArrows.length > 0) {
+    menuArrows.forEach((menuArrow, index) => {
+      menuArrow.addEventListener('click', () => {
+        menuArrow.classList.toggle('menu__arrow_active');
+        menuSublists[index].classList.toggle('menu__sublist_active');
       });
-    }
-  });
+    });
+  }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  hideShowMenu();
+  hideShowSpoiler();
+});
